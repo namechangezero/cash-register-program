cace6709dd502dd3fb47b3f4f11e795ed2b0faad
namechanged0 <alessandrokrauter@gmail.com>
2022-06-16 13:10:21 +0200

    moved files into seperate folder for better view

--- client/addProduct/addProduct.js
+++ client/addProduct/addProduct.js
@@ -7,6 +7,8 @@ var menuText = document.getElementsByClassName("menuText");
 
 var showAdded = document.getElementById("showAdded")
 
+var prodName = document.getElementById("prodName")
+var prodPrice = document.getElementById("prodPrice")
 
 function closeAddProduct(){
     addProd.style.display = "none"
@@ -15,7 +17,10 @@ function closeAddProduct(){
 function OpenAddProduct(){
     addProd.style.display = "block"
     showAdded.innerHTML = ""
-    
+
+    prodPrice.value=""
+    prodName.value=""
+
 
     openOrCloseMenu()
 
@@ -23,5 +28,13 @@ function OpenAddProduct(){
 }
 
 function addProduct(){
+    var price = prodPrice.value, name = prodName.value
+    if (price.indexOf("€")!=-1){
+        alert("Entferne das Eurozeichen im Preis. Der Preis ist bereits in Euro festgelegt.")
+        return
+    }
 
+    localStorage.setItem("product:"+name,price)
+    alert("Hinzugefügt!")
+    addProd.style.display="none"
 }
